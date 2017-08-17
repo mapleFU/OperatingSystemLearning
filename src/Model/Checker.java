01bf61283c94535198b9b371a7db3f68ad6aefd9
付旭炜 <1506118561@qq.com>
2017-08-17 21:58:29 +0800

    Update checker to make it runnable. Add CheckerManager.

--- src/Model/Checker.java
+++ src/Model/Checker.java
@@ -1,12 +1,15 @@
 package Model;
 
+import java.sql.Time;
+import java.util.Random;
+
 /**
  * 乘客安检的时间长短由随机数产生，范围在MinTimeLen到MaxTimeLen之间，
  * 安检口暂停休息时间长短由随机数产生，范围在MinRestTimeLen到MaxRestTimeLen之间。
  * MinTimeLen，MaxTimeLen，MinRestTimeLen，MaxRestTimeLen这四个值是系统可以配置的参数，
  * 保存在配置文件中，系统初始化时需要读取此参数
  */
-public class Checker {
+public class Checker extends Thread {
     // STATIC DATA
 
     public final static int MaxCheck = 6;   // 最多检查6人
@@ -32,16 +35,47 @@ public class Checker {
     }
 
     // private data
-    private int person = 0;         // 正在排队的数目
+    private int person;         // 正在排队的数目
     public synchronized void addPerson() {
         ++person;
+    }
+    public synchronized void popPerson() {
+        --person;
+        // DEBUG
+        if (person < 0) {
+            throw new RuntimeException("Person < 0!!!");
+        }
     }
 
     /**
      * @return person waiting in line
+     * TODO: find out if you need lock here
      */
-    public synchronized int getPerson() {
+    public int getPerson() {
         return person;
     }
 
+    Random random = new Random();
+
+    private int getRandomCheck() {
+        return random.nextInt(MaxTimeLen - MinTimeLen) + MinTimeLen;
+    }
+    private int getRandomRetire() {
+        return random.nextInt(MaxRestTimeLen - MinRestTimeLen) + MinRestTimeLen;
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            while (getPerson() == 0) {}      //if person == 0, block(Is it right?)
+            int waitTime = getRandomCheck();
+            try {
+                Thread.sleep(waitTime * 1000);
+            } catch (InterruptedException e) {
+                System.err.println("Error in sleep");
+            }
+            popPerson();
+        }
+    }
 }
